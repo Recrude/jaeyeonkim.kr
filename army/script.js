c90b32d295531a3fbc90a1976670839f29e7e044
Jaeyeon Kim <70138657+Recrude@users.noreply.github.com>
2024-10-01 00:39:30 +0900

    Add files via upload

--- army/script.js
+++ army/script.js
@@ -80,20 +80,19 @@
 
 
 var counter1 = function(){
-    var startDate = new Date("2024/09/29 00:00:00") //디데이 (날짜 설정)
+    var startDate = new Date("2024/09/30 22:05:00") //디데이 (날짜 설정)
         setInterval(function(){
             var currentDate = new Date(); //현재 날짜 가져오기
             var distance = startDate.getTime() - currentDate.getTime();
-            var d = String(Math.floor(distance / (1000 * 60 * 60 * 24)));
-            var h = String(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-            var m = String(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
-            var s = String(Math.floor((distance % (1000 * 60)) / 1000));
+            var d = Math.abs(String(Math.floor(distance / (1000 * 60 * 60 * 24))));
+            var h = Math.abs(String(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))));
+            var m = Math.abs(String(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))));
+            var s = Math.abs(String(Math.floor((distance % (1000 * 60)) / 1000)));
             if(d < 10){ d = '0'+d;}
             if(h < 10){ h = '0'+h;}
             if(m < 10){ m = '0'+m;}
             if(s < 10){ s = '0'+s;}
             $('#remain-time').html(d + ' ' + h +':'+ m +':'+ s+'' )
-            
             // $('.progress-bar').css({
             //     width: d * (widthbar / 31) +'px' //여기서 31은 1일부터 ~ 31일까지 기준이다.
             // })
@@ -147,7 +146,9 @@ var counter3 = function(){
 }
 counter3();
 setInterval(counter3, 1000);
+var startend = function(){
 
+}
 
 
 // $('#explain').click(
@@ -164,3 +165,57 @@ setInterval(counter3, 1000);
 //         remainTime3.classList.toggle('selected');
 //     }
 // )
+
+
+const Confettiful = function(el) {
+  this.el = el;
+  this.containerEl = null;
+  
+  this.confettiFrequency = 3;
+  this.confettiColors = ['#EF2964', '#00C09D', '#2D87B0', '#48485E','#EFFF1D'];
+  this.confettiAnimations = ['slow', 'medium', 'fast'];
+  
+  this._setupElements();
+  this._renderConfetti();
+};
+
+Confettiful.prototype._setupElements = function() {
+  const containerEl = document.createElement('div');
+  const elPosition = this.el.style.position;
+  
+  if (elPosition !== 'relative' || elPosition !== 'absolute') {
+    this.el.style.position = 'relative';
+  }
+  
+  containerEl.classList.add('confetti-container');
+  
+  this.el.appendChild(containerEl);
+  
+  this.containerEl = containerEl;
+};
+
+Confettiful.prototype._renderConfetti = function() {
+  this.confettiInterval = setInterval(() => {
+    const confettiEl = document.createElement('div');
+    const confettiSize = (Math.floor(Math.random() * 3) + 7) + 'px';
+    const confettiBackground = this.confettiColors[Math.floor(Math.random() * this.confettiColors.length)];
+    const confettiLeft = (Math.floor(Math.random() * this.el.offsetWidth)) + 'px';
+    const confettiAnimation = this.confettiAnimations[Math.floor(Math.random() * this.confettiAnimations.length)];
+    
+    confettiEl.classList.add('confetti', 'confetti--animation-' + confettiAnimation);
+    confettiEl.style.left = confettiLeft;
+    confettiEl.style.width = confettiSize;
+    confettiEl.style.height = confettiSize;
+    confettiEl.style.backgroundColor = confettiBackground;
+    
+    confettiEl.removeTimeout = setTimeout(function() {
+      confettiEl.parentNode.removeChild(confettiEl);
+    }, 3000);
+    
+    this.containerEl.appendChild(confettiEl);
+  }, 25);
+};
+
+window.confettiful = new Confettiful(document.querySelector('.js-container'));
+
+
